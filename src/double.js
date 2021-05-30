ffaa8ee76289d643316eb94fa4dabacfd5d32ddf
root <root@LAPTOP-NOSA984M.localdomain>
2021-05-30 12:59:04 +0900

    Install Gatsby

--- src/double.js
+++
@@ -1,5 +0,0 @@
-function double(x) {
-  return x * 2
-}
-
-module.exports = double
--- src/double.test.js
+++
@@ -1,5 +0,0 @@
-const double = require("./double")
-
-test('double 2 to wqual 4', () => {
-  expect(double(2)).toBe(4)
-})
