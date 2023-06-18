662028e88ae4ae78a5992fa5dc7e6da8198f9538
kento <kento@toriwatari.bird>
2023-06-18 12:42:54 +0900

    ⚡ Update

---
+++ README.md
@@ -0,0 +1,13 @@
+# Yew Todo App
+
+```shell
+cargo install trunk
+```
+
+```shell
+rustup target add wasm32-unknown-unknown
+```
+
+```shell
+trunk serve --open
+```
--- src/components/todo/todo_list.rs
+++ src/components/todo/todo_list.rs
@@ -1,30 +1,18 @@
+use crate::Properties;
 use yew::{function_component, html, Html};
 use crate::components::todo::todo_item::TodoItem;
 use crate::components::todo::types::Todo;
 
-#[function_component(TodoList)]
-pub fn todo_list() -> Html {
-    let todo_items = vec![
-        Todo {
-            id: 1,
-            title: String::from("TodoA"),
-            completed: true,
-        },
-        Todo {
-            id: 2,
-            title: String::from("TodoB"),
-            completed: true,
-        },
-        Todo {
-            id: 3,
-            title: String::from("TodoC"),
-            completed: false,
-        },
-    ];
+#[derive(Properties, PartialEq)]
+pub struct TodoItemProps {
+    pub todo_items: Vec<Todo>,
+}
 
+#[function_component(TodoList)]
+pub fn todo_list(props: &TodoItemProps) -> Html {
     html! {
         <ul class="list-group">
-            {todo_items.iter().map(|todo| html! {
+            {props.todo_items.iter().map(|todo| html! {
                 <TodoItem title={todo.title.clone()} completed={todo.completed} />
             }).collect::<Html>()}
         </ul>
